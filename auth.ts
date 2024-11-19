c22488a5ec94e2d8256d8f2b8eeb4c1477aebaba
Mahfud <mahfudmuhyiddin@gmail.com>
2024-11-20 01:23:10 +0700

    update commit

--- auth.ts
+++ auth.ts
@@ -1,11 +1,12 @@
 import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
-import bcrypt from 'bcrypt';
 import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
 
-async function getUser(email: string): Promise<User | undefined> {
+async function getUser(email: string): Promise<User | undefined>{
     try {
         const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
         return user.rows[0];
@@ -15,25 +16,20 @@ async function getUser(email: string): Promise<User | undefined> {
     }
 }
 
-export default NextAuth({
+export const { auth, signIn, signOut } = NextAuth({ 
+    ...authConfig,
     providers: [
         Credentials({
-            name: 'Credentials',
-            credentials: {
-                email: { label: 'Email', type: 'text' },
-                password: { label: 'Password', type: 'password' },
-            },
             async authorize(credentials) {
-                const parsedCredentials = z
-                    .object({ email: z.string().email(), password: z.string().min(6) })
-                    .safeParse(credentials);
+                const parsedCredentials = z.object({ email: z.string().email(), password: z.string().min(6) })
+                .safeParse(credentials);
 
                 if (parsedCredentials.success) {
                     const { email, password } = parsedCredentials.data;
                     const user = await getUser(email);
                     if (!user) return null;
-
                     const passwordsMatch = await bcrypt.compare(password, user.password);
+
                     if (passwordsMatch) return user;
                 }
 
@@ -41,5 +37,4 @@ export default NextAuth({
             },
         }),
     ],
-    secret: process.env.NEXTAUTH_SECRET, // Set your secret
 });
